f9f278609db3970dacf226eab754c48d85a2d837
Grisha Trubetskoy <grisha@modpython.org>
2015-09-28 17:52:32 -0400

    betterParseDuration support for mon (sort of)

--- config.go
+++ config.go
@@ -84,10 +84,17 @@ type trRRASpec struct {
 }
 
 func betterParseDuration(s string) (time.Duration, error) {
+
 	if strings.HasSuffix(s, "min") {
 		s = s[0 : len(s)-2] // min -> m
 	} else if strings.HasSuffix(s, "hour") {
 		s = s[0 : len(s)-3] // hour -> h
+	} else if strings.HasSuffix(s, "mon") {
+		fd, err := strconv.ParseFloat(s[0:len(s)-3], 64)
+		if err != nil {
+			return 0, err
+		}
+		s = fmt.Sprintf("%vh", fd*30*24)
 	}
 	if d, err := time.ParseDuration(s); err != nil {
 		if strings.HasPrefix(err.Error(), "time: unknown unit ") {
